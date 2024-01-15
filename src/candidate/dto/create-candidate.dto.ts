927b3f88c1bac5f27eff1b4a55bd842f0bf655b5
Osouzaa <gabrielsouzaalves750@gmail.com>
2024-01-15 13:11:01 -0300

    feat: uptade info

--- src/candidate/dto/create-candidate.dto.ts
+++ src/candidate/dto/create-candidate.dto.ts
@@ -1,52 +1,67 @@
-import {  IsNotEmpty, IsString } from 'class-validator';
+import { IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateCandidateDto {
+  @IsNotEmpty({ message: "O campo profissional não pode estar vazio" })
   @IsString()
   profissional: string;
 
+  @IsNotEmpty({ message: "O campo idade não pode estar vazio" })
   @IsString()
   idade: string;
 
+  @IsNotEmpty({ message: "O campo status não pode estar vazio" })
   @IsString()
   status: string;
 
-  @IsNotEmpty({message: "O campo de CPF não pode estar vazio"})
+  @IsNotEmpty({ message: "O campo de CPF não pode estar vazio" })
   @IsString()
   cpf: string;
 
+  @IsNotEmpty({ message: "O campo telefone não pode estar vazio" })
   @IsString()
   telefone: string;
 
+  @IsNotEmpty({ message: "O campo cidade não pode estar vazio" })
   @IsString()
   cidade: string;
 
+  @IsNotEmpty({ message: "O campo email não pode estar vazio" })
   @IsString()
   email: string;
 
+  @IsNotEmpty({ message: "O campo vaga 100% presencial Porto Real RJ não pode estar vazio" })
   @IsString()
   vaga_100_presencial_porto_real_rj: string;
 
+  @IsNotEmpty({ message: "O campo vaga 100% presencial Goiana PE não pode estar vazio" })
   @IsString()
   vaga_100_presencial_goiana_pe: string;
 
+  @IsNotEmpty({ message: "O campo vaga 100% presencial Betim MG não pode estar vazio" })
   @IsString()
   vaga_100_presencial_betim_mg: string;
 
+  @IsNotEmpty({ message: "O campo vaga híbrida Betim não pode estar vazio" })
   @IsString()
   vaga_hibrida_betim: string;
 
+  @IsNotEmpty({ message: "O campo home office não pode estar vazio" })
   @IsString()
   home_office: string;
 
+  @IsNotEmpty({ message: "O campo última empresa não pode estar vazio" })
   @IsString()
   ultima_empresa: string;
 
+  @IsNotEmpty({ message: "O campo último salário não pode estar vazio" })
   @IsString()
   ultimo_salario: string;
 
+  @IsNotEmpty({ message: "O campo target CLT não pode estar vazio" })
   @IsString()
   target_clt: string;
 
+  @IsNotEmpty({ message: "O campo conhecimento de inglês não pode estar vazio" })
   @IsString()
   conhecimento_ingles: string;
 }
--- src/database/entities/candidate.entity.ts
+++ src/database/entities/candidate.entity.ts
@@ -11,52 +11,52 @@ export class Candidate {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column({ nullable: true, type: 'varchar' })
+  @Column({ nullable: false, type: 'varchar' })
   profissional: string;
   
   @Column({ nullable: false, type: 'varchar', unique: true })
   cpf: string;
 
-  @Column({ nullable: true, type: 'varchar' })
+  @Column({ nullable: false, type: 'varchar' })
   idade: string;
 
-  @Column({ nullable: true, type: 'varchar' })
+  @Column({ nullable: false, type: 'varchar' })
   status: string;
 
-  @Column({ nullable: true, type: 'varchar' })
+  @Column({ nullable: false, type: 'varchar' })
   telefone: string;
   
-  @Column({ nullable: true, type: 'varchar' })
+  @Column({ nullable: false, type: 'varchar' })
   cidade: string;
 
-  @Column({ nullable: true, type: 'varchar' })
+  @Column({ nullable: false, type: 'varchar' })
   email: string;
 
-  @Column({ nullable: true, type: 'varchar' })
+  @Column({ nullable: false, type: 'varchar' })
   conhecimento_ingles: string;
 
-  @Column({ nullable: true, type: 'varchar' })
+  @Column({ nullable: false, type: 'varchar' })
   ultima_empresa: string;
 
-  @Column({ nullable: true, type: 'varchar' })
+  @Column({ nullable: false, type: 'varchar' })
   ultimo_salario: string;
 
-  @Column({ nullable: true, type: 'varchar' })
+  @Column({ nullable: false, type: 'varchar' })
   target_clt: string;
 
-  @Column({ nullable: true, type: 'varchar' })
+  @Column({ nullable: false, type: 'varchar' })
   vaga_100_presencial_porto_real_rj: string;
   
-  @Column({ nullable: true, type: 'varchar' })
+  @Column({ nullable: false, type: 'varchar' })
   vaga_100_presencial_goiana_pe: string;
 
-  @Column({ nullable: true, type: 'varchar' })
+  @Column({ nullable: false, type: 'varchar' })
   vaga_100_presencial_betim_mg: string;
 
-  @Column({ nullable: true, type: 'varchar' })
+  @Column({ nullable: false, type: 'varchar' })
   vaga_hibrida_betim: string;
    
-  @Column({ nullable: true, type: 'varchar' })
+  @Column({ nullable: false, type: 'varchar' })
   home_office: string;
 
 
