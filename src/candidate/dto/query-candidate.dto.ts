4e1e3cea918add9fd367fb265b5cf24f8501e942
Osouzaa <gabrielsouzaalves750@gmail.com>
2024-01-19 14:59:37 -0300

    feat: query dto

---
+++ src/candidate/dto/query-candidate.dto.ts
@@ -0,0 +1,20 @@
+
+export class QueryCandidateDto {
+  profissional?: string;
+  idade?: string;
+  status?: string;
+  cpf?: string;
+  telefone?: string;
+  cidade?: string;
+  email?: string;
+  vaga_100_presencial_porto_real_rj?: string;
+  vaga_100_presencial_goiana_pe?: string;
+  vaga_100_presencial_betim_mg?: string;
+  vaga_hibrida_betim?: string;
+  home_office?: string;
+  ultima_empresa?: string;
+  ultimo_salario?: string;
+  target_clt?: string;
+  conhecimento_ingles?: string;
+  observacao?: string;
+}
