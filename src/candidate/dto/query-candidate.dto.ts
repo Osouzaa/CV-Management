febe6f105f8481980497643b9b7d02128fb819e9
Osouzaa <gabrielsouzaalves750@gmail.com>
2024-01-29 08:44:08 -0300

    feat: new query options

--- src/candidate/dto/query-candidate.dto.ts
+++ src/candidate/dto/query-candidate.dto.ts
@@ -1,7 +1,7 @@
-
 export class QueryCandidateDto {
   profissional?: string;
-  idade?: string;
+  minIdade?: string;
+  maxIdade?: string;
   status?: string;
   cpf?: string;
   telefone?: string;
@@ -10,11 +10,26 @@ export class QueryCandidateDto {
   vaga_100_presencial_porto_real_rj?: string;
   vaga_100_presencial_goiana_pe?: string;
   vaga_100_presencial_betim_mg?: string;
+  vaga_internacional?: string;
   vaga_hibrida_betim?: string;
   home_office?: string;
   ultima_empresa?: string;
   ultimo_salario?: string;
-  target_clt?: string;
+  pretensao_salarial?: string;
   conhecimento_ingles?: string;
   observacao?: string;
+  cnpj?: string;
+  esta_empregado?: string;
+  empresa_atual?: string;
+  experiencia_ramo_automotivo?: string;
+  modalidade_atual?: string;
+  tipo_desejado_linkedin?: string;
+  nivel_funcao?: string;
+  formacao?: string;
+  interesse_imediato?: string;
+  entrevista_online?: string;
+  teste_tecnico?: string;
+  pretensao_pj?: string;
+  tipo_cnpj?: string;
+  foi_avaliado_recrutamento?: boolean;
 }
